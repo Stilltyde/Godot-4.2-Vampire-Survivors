5dd07b9e4f968e408b0d03ec56ff071d6933d79b
Stilltyde <zacv112@mail.ru>
2025-07-08 19:23:47 +0300

    game stop on 0 health

--- gun.gd
+++ gun.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@warning_ignore("unused_parameter")
 func _physics_process(delta: float) -> void:
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
--- slimie.gd
+++ slimie.gd
@@ -1,7 +1,12 @@
 extends CharacterBody2D
 
+@warning_ignore("unused_parameter")
+
 var health = 3
 @onready var player = get_node("/root/Game/Player") 
+
+func _ready():
+	%Slime.play_walk()
 
 func _physics_process(delta: float) -> void:
 	var direction = global_position.direction_to(player.global_position)
@@ -14,4 +19,8 @@ func take_damage():
 	
 	if health == 0:
 		queue_free()
+		const SMOKE = preload("res://smoke_explosion/smoke_explosion.tscn")
+		var smoke_explosion = SMOKE.instantiate()
+		get_parent().add_child(smoke_explosion)
+		smoke_explosion.global_position = global_position
  
