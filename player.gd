aec3f899ada3c97c66ea7d36b8d028231d12de77
Stilltyde <zacv112@mail.ru>
2025-07-08 18:02:15 +0300

    shooting done

--- player.gd
+++ player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@warning_ignore("unused_parameter")
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")	
 	velocity  = direction * 500
